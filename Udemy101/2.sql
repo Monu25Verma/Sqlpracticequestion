b75aebe633ea565134156ac199fe9fb9dd5af803
Monu25Verma <monuv2397@gmail.com>
2023-12-30 19:58:11 +0530

    2nd question in sql

--- Udemy101/2.sql
+++ Udemy101/2.sql
@@ -1,24 +1,43 @@
-#Question 2:
-#a. Write a SQL Statement that will show a count of schemas, tables, and columns (do not include views) in the AdventureWorks database.
+# Q2 a. Write a SQL Statement that will show a count of schemas, tables, and columns (do not include views) in the AdventureWorks database.
 
-select * from sys.tables
+b. Write a similar statement as part a but list each schema, table, and column (do not include views). This table can be used later in the course.
+
+
+use AdventureWorks2019
+select  *  from sys.tables
 select * from sys.columns
 select * from sys.schemas
 
 
-select count(Distinct s.name) as schemaName,
-	   count(Distinct t.name) as tableName,
-	   count(c.name) as columnName
+select 
+	count(distinct t.name) AS tableName
+	,count(distinct s.name) AS SchemaName
+	,count(c.name) AS columnName
 
-from   sys.tables  t
-INNER join  sys.columns  c on c.object_id = t.object_id
-INNER join  sys.schemas s  on s.schema_id = t.schema_id
+from 
+	sys.tables t inner join sys.columns c on c.object_id = t.object_id
+	inner join sys.schemas s on s.schema_id = t.schema_id
+ 
+
+ select
+	count(distinct Table_schema) as schemaName,
+	count(distinct table_name) as TableName,
+	count(COLUMN_NAME) as ColumnName
+ from INFORMATION_SCHEMA.COLUMNS where table_name not in(select  distinct Table_Name from INFORMATION_SCHEMA.views)
 
 
-#b. Write a similar statement as part a but list each schema, table, and column (do not include views). This table can be used later in the course.
+ select 
+	t.name AS tableName,
+	 s.name AS SchemaName
+	,c.name AS columnName
 
-select
-	count(distinct TABLE_SCHEMA) as tableschema,
-	count(distinct TABLE_NAME) as tableschema,
-	count(COLUMN_NAME) as tableschema
-from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME not in (select TABLE_NAME from INFORMATION_SCHEMA.VIEWS)
+from 
+	sys.tables t inner join sys.columns c on c.object_id = t.object_id
+	inner join sys.schemas s on s.schema_id = t.schema_id
+ 
+
+ select
+	 Table_schema as schemaName,
+	 table_name as TableName,
+	 COLUMN_NAME as ColumnName
+ from INFORMATION_SCHEMA.COLUMNS where table_name not in(select  distinct Table_Name from INFORMATION_SCHEMA.views)
